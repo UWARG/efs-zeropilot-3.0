93a6a63293731b65c350786a5ebc1b15dc2fc1cc
Christopher Chung <tps90067@gmail.com>
2022-10-22 18:23:08 -0500

    update main.c

--- main.c
+++ main.c
@@ -8,11 +8,10 @@ int main()
     
     while(1)
     {
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-//        HAL_Delay(1000);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+        HAL_Delay(1000);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-//        HAL_Delay(1000);
-
+        HAL_Delay(1000);
     }
 
     return 0;
