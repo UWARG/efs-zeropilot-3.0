48b97167c732eb5edd39a851867c31c7ebb3c0af
Christopher Chung <tps90067@gmail.com>
2022-10-30 16:55:25 -0500

    checkout updated LaminarOS branch, create blinky thread in ZP software instead of LOS repo

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "cmsis_os.h"
+
+osThreadId blinkyTestHandle;
+
+void StartBlinkyTest(void const * argument);
+
 
 int main()
 {
+
+    osThreadDef(blinkyTest, StartBlinkyTest, osPriorityNormal, 0, 128);
+    blinkyTestHandle = osThreadCreate(osThread(blinkyTest), NULL);
 
     losInit();
 
@@ -9,4 +20,15 @@ int main()
     while(1){}
 
     return 0;
+}
+
+
+void StartBlinkyTest(void const * argument)
+{
+    for(;;)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_Delay(250);
+        osDelay(1);
+    }
 }
