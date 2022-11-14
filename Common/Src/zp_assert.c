31e5e8b051a872e94be5aa9e3b9bb69be845870f
Aidan-B <abowers@uwaterloo.ca>
2022-11-14 00:00:26 -0500

    assert should check for failure

--- Common/Src/zp_assert.c
+++ Common/Src/zp_assert.c
@@ -1,7 +1,7 @@
 #include "zp_assert.h"
 
 void zp_assert(bool expr) {
-    if (expr) {
+    if (!expr) {
 #warning This should call the los hardfault handler, or something more appropriate
         while (1) {
         }
