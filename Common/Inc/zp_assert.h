0161dfdaf15a4315fd7d05a505134d7b7d35ffc9
Hardy Yu <h324yu@uwaterloo.ca>
2022-11-16 20:50:08 -0500

    First prototype of assertion

--- Common/Inc/zp_assert.h
+++ Common/Inc/zp_assert.h
@@ -1,8 +1,29 @@
+#pragma once
+
 #ifndef ZP_ASSERT_H
 #define ZP_ASSERT_H
 
-#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct sAssertInfo {
+    uint64_t pc;
+    uint64_t lr;
+} sAssertInfo;
+
+void store_assert(const uint64_t *pc, const uint64_t *lr);
+
+#define GET_LR() __builtin_return_address(0)
+//#define GET_PC(_a) __asm volatile ("mov %0, pc" : "=r" (_a)) // Comment out this for ARM
+#define GET_PC(_a) __asm volatile("1: lea 1b(%%rip), %0;": "=a"(pc));  // Comment this for x86
+
+#define MY_ASSERT_RECORD()       \
+    do {                         \
+      void *pc = NULL;           \
+      GET_PC(pc);            	 \
+      const void *lr = GET_LR(); \
+      store_assert(pc, lr);         \
+    } while (0)
 
 void zp_assert(bool expr);
 
-#endif  // ASSERT_H
+#endif
--- Common/Src/zp_assert.c
+++ Common/Src/zp_assert.c
@@ -1,9 +1,17 @@
 #include "zp_assert.h"
 
+static sAssertInfo g_assert_info;
+
+void store_assert(const uint64_t *pc, const uint64_t *lr) {
+    g_assert_info.pc = pc;
+    g_assert_info.lr = lr;
+}
+
 void zp_assert(bool expr) {
     if (!expr) {
-#warning This should call the los hardfault handler, or something more appropriate
+//#warning This should call the los hardfault handler, or something more appropriate
         while (1) {
+        	MY_ASSERT_RECORD();
         }
     }
 }
