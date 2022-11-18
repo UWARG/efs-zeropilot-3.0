aa6becf40bed9d63d183a87be9d4eff6d2b00a44
Gordon Fountain <Gordonthecellist@gmail.com>
2022-11-17 22:51:33 -0500

    Build up thread work improve modes

--- SystemManager/Src/SM_States.cpp
+++ SystemManager/Src/SM_States.cpp
@@ -12,15 +12,19 @@
 /********************
  * Boot Mode
  ********************/
-void BootMode::enter(SystemManager *sys_man) {
+void BootMode::execute(SystemManager *sys_man) {
     // Only Executes once, Configure sensors, LOS, Everything startup
+
+    los_interface.init(losInitData); // TODO, make this agree with what LOS needs
+
     // Start TM at slow speed
+    xTaskCreate(sys_man.TMSlowTask, "TM Slow Thread", &sys_man.TM_handle);
+    
+    vTaskStartScheduler(); // TODO, make sure I'm using this right, haven't checked
 
     // Update TM mail queue ID
-    TM_to_SM_queue = osMailCreate();
-}
+    sys_man.TM_to_SM_queue = osMailCreate();
 
-void BootMode::execute(SystemManager *sys_man) {
     // Immediately move to Disarm mode
     sys_man->setState(DisarmMode::getInstance());
 }
@@ -35,8 +39,11 @@ SystemState &BootMode::getInstance() {
  ********************/
 void DisarmMode::enter(SystemManager *sys_man) {
     // Start AM to accept PID tunings
-    // TODO
-    SM_to_AM_queue = osMailCreate();
+    xTaskCreate(sys_man.AMOperationTask, "AM Thread", &sys_man.AM_handle);
+
+    vTaskStartScheduler(); // TODO, make sure I'm using this right, haven't checked
+
+    sys_man.SM_to_AM_queue = osMailCreate();
 }
 
 void DisarmMode::execute(SystemManager *sys_man) {
@@ -62,8 +69,8 @@ void GroundOpMode::execute(SystemManager *sys_man) {
 
 void GroundOpMode::exit(SystemManager *sys_man) {
     // Delete SM to AM queue to prevent PID tuning midair
-    osMailFree(SM_to_AM_queue);
-    SM_to_AM_queue = NULL;
+    osMailFree(sys_man.SM_to_AM_queue);
+    sys_man.SM_to_AM_queue = NULL;
 }
 
 SystemState &GroundOpMode::getInstance() {
@@ -75,27 +82,33 @@ SystemState &GroundOpMode::getInstance() {
  * Flight Mode
  ********************/
 void FlightMode::enter(SystemManager *sys_man) {
-    // Start the PM thread and increase TM speed
-    // TODO
-    SM_to_PM_queue = osMailCreate();
-    PM_to_AM_queue = osMailCreate();
+    // Start the PM thread and increase TM speed (have to start different task)
+    xTaskCreate(sys_man.PMOperationTask, "PM Thread", &sys_man.PM_handle);
+    vTaskDelete(sys_man.TM_handle);
+    xTaskCreate(sys_man.TMOperationTask, "TM Thread", &sys_man.TM_handle);
+
+    sys_man.SM_to_PM_queue = osMailCreate();
+    sys_man.PM_to_AM_queue = osMailCreate();
+
+    vTaskStartScheduler(); // TODO, make sure I'm using this right, haven't checked
 }
 
 void FlightMode::execute(SystemManager *sys_man) {
+    // Get sensor fusion data from LOS interfaces
+    // TODO
+    // SM.sf_data = los_interface.
+
     // Get big packet message from TM and store it
     // TODO
 
-    // Determine flight state (Takeoff/cruise/Landing) and update state
+    // Determine flight state (Takeoff/cruise/Landing/landed) and update state
     // TODO
 
-    // If disarm then move to disarm
+    // If disarm/landed then move to disarm
     // sys_man->setState(DisarmMode::getInstance());
 
-    // If fatal Failure then move to disarm
+    // If fatal Failure then move to fatal fail
     // sys_man->setState(FatalFailMode::getInstance());
-
-    // Get sensor fusion data from LOS interfaces
-    // TODO
     
     // Get waypoint(s) from TM/RC (through LOS) and pass SF data and waypoint(s) to PM (who sends to AM)
     // TODO
@@ -105,14 +118,29 @@ void FlightMode::execute(SystemManager *sys_man) {
 
     // Create to-ground packet with AM response and SF and send through mail queue to TM
     // TODO
+
 }
 
 void FlightMode::exit(SystemManager *sys_man) {
-    // Stop the PM thread and decrease TM speed
-    osMailFree(SM_to_PM_queue);
-    SM_to_PM_queue = NULL;
-    osMailFree(PM_to_AM_queue);
-    PM_to_AM_queue = NULL;
+    // Stop the AM, PM thread and decrease TM speed
+
+    // Kill PM
+    vTaskDelete(sys_man.PM_handle);
+    // Kill AM
+    vTaskDelete(sys_man.AM_handle);
+
+    // Slow TM operation speed
+    vTaskDelete(sys_man.TM_handle);
+    xTaskCreate(sys_man.TMSlowTask, "TM Slow Thread", &sys_man.TM_handle);
+
+    vTaskStartScheduler(); // TODO, make sure I'm using this right, haven't checked
+
+    osMailFree(sys_man.SM_to_PM_queue);
+    sys_man.SM_to_PM_queue = NULL;
+    osMailFree(sys_man.PM_to_AM_queue);
+    sys_man.PM_to_AM_queue = NULL;
+
+    // Write 0 to LOS actuators
     // TODO
 }
 
@@ -121,30 +149,24 @@ SystemState& FlightMode::getInstance() {
     return singleton;
 }
 
-inputs_to_AM_t* FlightMode::generateWaypoint() {
-    // Generate a waypoint to be sent to PM
-    if (telemetryMsg.manual_control) {
-        AM_Waypoints = getManualWaypoint();
-    } else {
-        AM_Waypoints = getPathManagerWaypoint();
-    }
-    return AM_Waypoints;
-}
-
 inputs_to_AM_t* FlightMode::getManualWaypoint() {
-    // Convert manual pilot controls to AM waypoint
+    // Convert manual pilot controls from RC to AM waypoint
     // TODO
     return 0;
 }
 
-inputs_to_AM_t* FlightMode::getPathManagerWaypoint() {
-    // Create the waypoint(s) to pass to path manager
-    // TODO
-    return 0;
-}
-
-CommandsFromSM* FlightMode::generatePMPacket() {
+CommandsFromSM* FlightMode::generatePMPacket(SystemManager *sys_man) {
     // Pack SF and Waypoint data to packet sent to PM
+    CommandsFromSM pm_packet;
+
+    pm_packet = sys_man.sf_data;
+
+    if (sys_man.RC_new_data) { // TODO make this match what we get
+        sys_man.AM_Waypoints = getManualWaypoint(sys_man.RC_msg);
+    } 
+    
+    
+
     //TODO
     return 0;
 }
@@ -156,14 +178,19 @@ CommandsFromSM* FlightMode::generatePMPacket() {
 
 void FatalFailureMode::enter(SystemManager *sys_man) {
     // Kill PM
+    vTaskDelete(sys_man.PM_handle);
     // Kill AM
+    vTaskDelete(sys_man.AM_handle);
 
-    osMailFree(SM_to_PM_queue);
-    SM_to_PM_queue = NULL;
-    osMailFree(PM_to_AM_queue);
-    PM_to_AM_queue = NULL;
-    osMailFree(PM_to_AM_queue);
-    AM_to_SM_queue = NULL;
+    vTaskStartScheduler(); // TODO, make sure I'm using this right, haven't checked
+    
+
+    osMailFree(sys_man.SM_to_PM_queue);
+    sys_man.SM_to_PM_queue = NULL;
+    osMailFree(sys_man.PM_to_AM_queue);
+    sys_man.PM_to_AM_queue = NULL;
+    osMailFree(sys_man.PM_to_AM_queue);
+    sys_man.AM_to_SM_queue = NULL;
 
     // Write 0 to LOS actuators
     // TODO
