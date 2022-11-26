b80bed0232ac565533399fb92257800c3871c663
GuyBlumenthal <guy.blumenthal@outlook.com>
2022-11-25 19:54:24 -0500

    rtos things

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "FreeRTOS.h"
 #include "cmsis_os2.h"
 #include "task.h"
-#include "cmsis_os.h"
 #include "LOS_Actuators.hpp"
 
 osThreadId blinkyTestHandle;
@@ -14,13 +13,39 @@ void StartLosActuatorsTest(void const * argument);
 
 int main()
 {
+    
+/// Attributes structure for thread.
+typedef struct {
+  const char                   *name;   ///< name of the thread
+  uint32_t                 attr_bits;   ///< attribute bits
+  void                      *cb_mem;    ///< memory for control block
+  uint32_t                   cb_size;   ///< size of provided memory for control block
+  void                   *stack_mem;    ///< memory for stack
+  uint32_t                stack_size;   ///< size of stack
+  osPriority_t              priority;   ///< initial thread priority (default: osPriorityNormal)
+  TZ_ModuleId_t            tz_module;   ///< TrustZone module identifier
+  uint32_t                  reserved;   ///< reserved (must be 0)
+} osThreadAttr_t;osThreadId_t osThreadNew (osThreadFunc_t func, void *argument, const osThreadAttr_t *attr);
+
     /* Init scheduler */
     osKernelInitialize();
 
-    osThreadDef(blinkyTest, StartBlinkyTest, osPriorityNormal, 0, 128);
-    osThreadDef(losActuatorsTest, StartLosActuatorsTest, osPriorityNormal, 0, 1024);
-    blinkyTestHandle = osThreadCreate(osThread(blinkyTest), NULL);
-    losActuatorsTestHandle = osThreadCreate(osThread(losActuatorsTest), NULL);
+    blinkyTestHandle = osThreadCreate();
+
+    osThreadAttr_t blinkyTest = {
+        .name = "start_blinky",
+        .stack_size = 128,
+        .priority = osPriorityNormal
+    };
+
+    osThreadAttr_t losActTest = {
+        .name = "los_act",
+        .stack_size = 1024,
+        .priority = osPriorityNormal
+    };
+
+    osThreadNew (blinkyTest, NULL, &blinkyTest);
+    osThreadNew (losActuatorsTest, NULL, &losActuatorsTest);
 
     losInit();
 
