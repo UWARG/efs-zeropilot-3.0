2048376a7b3c4ae3638b235dc64b6f6a8919df51
Aaditya Chaudhary <bluebird1339@gmail.com>
2022-12-10 04:43:01 +0900

    committing failed test on purpose

--- TelemetryManager/tests/ExampleTest.cpp
+++ TelemetryManager/tests/ExampleTest.cpp
@@ -5,5 +5,5 @@ TEST(TelemetryManager, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(7 * 6, 41);
 }
