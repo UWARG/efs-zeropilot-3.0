19f382252a9ab1543e0dd8d7fb9babf56dddb803
Aidan Bowers <abowers@uwaterloo.ca>
2023-05-20 19:03:00 -0400

    Ensure GPIO and UART work
    - UART4 and USART1 haven't been tested with control flow
    (they work without control flow)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "main.h"
 
+#include <cstring>
+
 #include "FreeRTOS.h"
 #include "LOS_Actuators.hpp"
 #include "SM.hpp"
 #include "cmsis_os2.h"
 #include "task.h"
+#include "usart.h"
+
+void GPIOTask(void *pvParameters);
+void UARTTask(void *pvParameters);
 
 void SMOperationTask(void *pvParameters);
 const static auto SM_PERIOD_MS = 5;
@@ -12,9 +18,14 @@ const static auto SM_PERIOD_MS = 5;
 int main() {
     losInit();
 
-    TaskHandle_t SM_handle = NULL;
+    // TaskHandle_t SM_handle = NULL;
+    // xTaskCreate(SMOperationTask, "SM Thread", 400U, NULL, osPriorityNormal, &SM_handle);
 
-    xTaskCreate(SMOperationTask, "SM Thread", 400U, NULL, osPriorityNormal, &SM_handle);
+    TaskHandle_t hGPIO = NULL;
+    xTaskCreate(GPIOTask, "GPIO", 50U, NULL, osPriorityNormal, &hGPIO);
+
+    TaskHandle_t hUART = NULL;
+    xTaskCreate(UARTTask, "UART", 500U, NULL, osPriorityNormal, &hUART);
 
     losKernelStart();
 
@@ -33,5 +44,61 @@ void SMOperationTask(void *pvParameters) {
     while (true) {
         SM_instance.execute();
         vTaskDelayUntil(&xNextWakeTime, SM_PERIOD_MS);
+    }
+}
+
+void GPIOTask(void *pvParameters) {
+    TickType_t xNextWakeTime;
+    xNextWakeTime = xTaskGetTickCount();
+
+    while (true) {
+        HAL_GPIO_TogglePin(GPIO_1_GPIO_Port, GPIO_1_Pin);
+        HAL_GPIO_TogglePin(GPIO_2_GPIO_Port, GPIO_2_Pin);
+        HAL_GPIO_TogglePin(GPIO_3_GPIO_Port, GPIO_3_Pin);
+        HAL_GPIO_TogglePin(GPIO_4_GPIO_Port, GPIO_4_Pin);
+        HAL_GPIO_TogglePin(GPIO_5_GPIO_Port, GPIO_5_Pin);
+        HAL_GPIO_TogglePin(GPIO_6_GPIO_Port, GPIO_6_Pin);
+        HAL_GPIO_TogglePin(GPIO_7_GPIO_Port, GPIO_7_Pin);
+        HAL_GPIO_TogglePin(GPIO_8_GPIO_Port, GPIO_8_Pin);
+        HAL_GPIO_TogglePin(GPIO_9_GPIO_Port, GPIO_9_Pin);
+        HAL_GPIO_TogglePin(GPIO_10_GPIO_Port, GPIO_10_Pin);
+
+        vTaskDelayUntil(&xNextWakeTime, 500);
+    }
+}
+
+void UARTTask(void *pvParameters) {
+    TickType_t xNextWakeTime;
+    xNextWakeTime = xTaskGetTickCount();
+
+    uint8_t LPUART1_Buffer[64] = {0};
+    uint8_t USART1_Buffer[64] = {0};
+    uint8_t USART2_Buffer[64] = {0};
+    uint8_t USART3_Buffer[64] = {0};
+    uint8_t UART4_Buffer[64] = {0};
+
+    while (true) {
+        HAL_UART_Receive(&hlpuart1, LPUART1_Buffer, 64, 80);
+        HAL_UART_Transmit(&hlpuart1, LPUART1_Buffer, 64, 10);
+
+        HAL_UART_Receive(&huart1, USART1_Buffer, 64, 80);
+        HAL_UART_Transmit(&huart1, USART1_Buffer, 64, 10);
+
+        HAL_UART_Receive(&huart2, USART2_Buffer, 64, 80);
+        HAL_UART_Transmit(&huart2, USART2_Buffer, 64, 10);
+
+        HAL_UART_Receive(&huart3, USART3_Buffer, 64, 80);
+        HAL_UART_Transmit(&huart3, USART3_Buffer, 64, 10);
+
+        HAL_UART_Receive(&huart4, UART4_Buffer, 64, 80);
+        HAL_UART_Transmit(&huart4, UART4_Buffer, 64, 10);
+
+        memset(LPUART1_Buffer, 0, sizeof(LPUART1_Buffer));
+        memset(USART1_Buffer, 0, sizeof(USART1_Buffer));
+        memset(USART2_Buffer, 0, sizeof(USART2_Buffer));
+        memset(USART3_Buffer, 0, sizeof(USART3_Buffer));
+        memset(UART4_Buffer, 0, sizeof(UART4_Buffer));
+
+        vTaskDelayUntil(&xNextWakeTime, 100);
     }
 }
